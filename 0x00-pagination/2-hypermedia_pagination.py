ea0ffed1f85d8d4e40e6c577a1b6580fc07ff669
maiyo008 <tonymaiyo008@gmail.com>
2023-07-23 12:59:28 +0300

    Fetch pagination hypermedia

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-1-simple_pagination.py
+2-hypermedia_pagination.py
 """
 import csv
 import math
@@ -65,3 +65,33 @@ class Server:
             return []
         page_data = dataset[start_index:end_index]
         return page_data
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        Fetch pagination hypermedia
+
+        Args:
+            page(int): The current page number (1-indexed).
+            page_size(int): The number of items per page.
+
+        Returns:
+            Dictionary with hypermedia info
+        """
+        assert isinstance(page, int) and page > 0,\
+            "Page must be a positive number"
+        assert isinstance(page_size, int) and page > 0,\
+            "Page_size must be a positive number"
+        data = self.get_page(page, page_size)
+        total_records = len(self.dataset())
+        total_pages = math.ceil(total_records/page_size)
+        start_index, end_index = index_range(page, page_size)
+        next_page = page + 1 if end_index < total_records else None
+        prev_page = page - 1 if page > 1 else None
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
