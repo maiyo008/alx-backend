8e7b7cd8994c27de7a037de69297b20c4f9c0cc6
maiyo008 <tonymaiyo008@gmail.com>
2023-09-27 07:25:36 +0300

    Hot fix- connecting to Redis using NodeJs client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,14 +1,12 @@
-import {createClient} from 'redis';
+import { createClient } from 'redis';
 
-async function connectToRedis() {
-    try{
-        const client = await createClient();
-            client.on('error', err => console.log("Redis client not connected to the server: ",err.message));
-            client.on('connect', () => console.log("Redis client connected to the server"));
-            //console.log('Redis client connected to the server');
-    } catch (err) {
-        console.error(`Redis client not connected to the server: ${err}`);
-    }
-};
+const client = createClient();
 
-connectToRedis();
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+  client.quit();
+});
