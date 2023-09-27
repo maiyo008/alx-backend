3829ad36d9525e8dc522a9a3ee22b83dce8d5e36
maiyo008 <tonymaiyo008@gmail.com>
2023-09-27 08:58:36 +0300

    Create the job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,25 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+    phoneNumber: '1234567890',
+    message: 'Hello, this is a test notification.'
+};
+
+const job = queue.create('push_notification_code', jobData)
+    .save((err) => {
+        if (!err) {
+            console.log(`Notification job created: ${job.id}`);
+        } else {
+            console.error(`Error creating job: ${err}`);
+        }
+    });
+
+job.on('complete', () => {
+    console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+    console.log('Notification job failed');
+});
